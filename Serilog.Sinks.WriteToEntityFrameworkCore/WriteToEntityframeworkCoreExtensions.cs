464c0c46cf55a4f976ee89b175b1aa26e56da9da
Entity-now <2966388213@qq.com>
2023-08-28 16:20:57 +0800

    add file

---
+++ Serilog.Sinks.WriteToEntityFrameworkCore/WriteToEntityframeworkCoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.WriteToEntityFrameworkCore
+{
+    using System;
+
+    using global::Serilog;
+    using global::Serilog.Configuration;
+    using Microsoft.EntityFrameworkCore;
+    using Serilog.Context;
+
+    public static class WriteToEntityframeworkCoreExtensions
+    {
+        public static LoggerConfiguration EntityFrameworkSink(
+          this LoggerSinkConfiguration loggerConfiguration,
+          Func<DbContext> dbContextProvider,
+          IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new WriteToEntityFrameworkCore(dbContextProvider, formatProvider));
+        }
+    }
+}
